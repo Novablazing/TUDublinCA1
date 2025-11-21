da89e6eb1adf6ee7e518bf73b5d439bcb4882fd0
Vyshnav A K <vyshnavak47@gmail.com>
2025-11-21 23:45:06 +0000

    Specflow

---
+++ SpecFlowTests/Steps/BloodPressureSteps.cs
@@ -0,0 +1,31 @@
+using System;
+using TechTalk.SpecFlow;
+using Xunit;
+using BPCalculator;
+
+namespace SpecFlowTests.Steps
+{
+    [Binding]
+    public class BloodPressureSteps
+    {
+        private BloodPressure _bp;
+
+        [Given(@"a systolic reading of (.*) and diastolic reading of (.*)")]
+        public void GivenASystolicReadingOfAndDiastolicReadingOf(int systolic, int diastolic)
+        {
+            _bp = new BloodPressure { Systolic = systolic, Diastolic = diastolic };
+        }
+
+        [When("the blood pressure is evaluated")]
+        public void WhenTheBloodPressureIsEvaluated()
+        {
+            // evaluation happens via properties on BloodPressure; nothing to do
+        }
+
+        [Then("the category should be (.*)")]
+        public void ThenTheCategoryShouldBe(string expectedCategory)
+        {
+            Assert.Equal(expectedCategory, _bp.Category.ToString());
+        }
+    }
+}
